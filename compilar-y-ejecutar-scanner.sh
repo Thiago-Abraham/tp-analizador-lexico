3a1a66c26a9f9f247fc57f83c79cffd09ef5a07b
Thiago Joaquin Abraham <66148060+Thiago-Abraham@users.noreply.github.com>
2026-05-29 08:51:15 -0300

    Modificacion del archivo que compila y ejecuta el scanner
    Se agrego una variable que le quita la extension .l del nombre del archivo para poder usar ese nombre en el .exe

--- compilar-y-ejecutar-scanner.sh
+++ compilar-y-ejecutar-scanner.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-read -p "Introduce el nombre del archivo scanner: " nombreScanner
+read -p "Introduce el nombre del archivo scanner (con .l): " nombreScanner
 
 set -e && echo "\nERROR AL COMPILAR\n"
 
+nombreSinExtension="${nombreScanner%.l}"
+
 flex $nombreScanner
-gcc lex.yy.c -o scanner
-./scanner < entrada.txt  
+gcc lex.yy.c -o $nombreSinExtension
+./$nombreSinExtension < entrada.txt  
