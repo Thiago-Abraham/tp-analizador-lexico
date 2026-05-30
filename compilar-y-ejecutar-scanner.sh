255c0d55f6a8add37042c68247dbf9321d23dadc
Thiago Joaquin Abraham <66148060+Thiago-Abraham@users.noreply.github.com>
2026-05-30 00:34:17 -0300

    Mejora de archivo compilar-y-ejecutar-scanner.sh y control de error en scanner.l
    Se agrego variable de entrada al archivo compiñar-y-ejecutar-scanner.sh y tambien un manejo de errores respecto a la compilacion.
    Tambien se modifico el comentario de scanner.l que describia <tokenNoReconocido> porque por alguna razon flex no toma bien los comentarios en la seccion %% %%.

--- compilar-y-ejecutar-scanner.sh
+++ compilar-y-ejecutar-scanner.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 read -p "Introduce el nombre del archivo scanner (con .l): " nombreScanner
-
-set -e && echo "\nERROR AL COMPILAR\n"
+read -p "Introduce el nombre del archivo de entrada (sin .txt): " nombreEntrada
 
 nombreSinExtension="${nombreScanner%.l}"
+nombreEntradaConExtension="${nombreEntrada}.txt"
 
-flex $nombreScanner
-gcc lex.yy.c -o $nombreSinExtension
-./$nombreSinExtension < entrada.txt  
+flex $nombreScanner && gcc lex.yy.c -o $nombreSinExtension || { echo -e "\nERROR AL COMPILAR\n"; exit 1; }
+
+./$nombreSinExtension < $nombreEntradaConExtension || { echo -e "\nERROR AL ABRIR ARCHIVO DE ENTRADA\n"; exit 1; }
