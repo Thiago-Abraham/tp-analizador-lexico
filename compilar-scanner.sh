5fc382640bf94287d029cd1a7b383dd47968ddd9
Thiago Joaquin Abraham <66148060+Thiago-Abraham@users.noreply.github.com>
2026-05-30 20:57:31 -0300

    Finalizacion de todas las reglas de erx
    Se agregaron las erx de constantes reales, constantes enteras, constantes de caracter, literales cadena y palabras reservadas
    
    Co-authored-by: JuanSequeiraDev <jusequeira@frba.utn.edu.ar>
    Co-authored-by: valensantarone <vsantarone@frba.utn.edu.ar>
    Co-authored-by: Tiago-Perez-Rios <tperezrios@frba.utn.edu.ar>

--- compilar-scanner.sh
+++ compilar-scanner.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-read -p "Introduce el nombre del archivo scanner (con .l): " nombreScanner
-
-flex $nombreScanner && gcc lex.yy.c -o $nombreSinExtension || { echo -e "\nERROR AL COMPILAR\n"; exit 1; }
+flex scanner.l && gcc lex.yy.c -o scanner || { echo -e "\nERROR AL COMPILAR\n"; exit 1; }
